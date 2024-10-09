509c0e1468adb5f5e8093a0672da4f3939e6fcc2
Noe Cortez <corteznoe3@gmail.com>
2024-10-09 11:55:49 -0600

    feat: :sparkles: Agrega vue al proyecto

---
+++ main.ts
@@ -0,0 +1,6 @@
+import './src/style.css'
+
+import { createApp } from 'vue'
+import App from './src/App.vue'
+
+createApp(App).mount('#app')
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { onMounted, useTemplateRef } from 'vue';
+
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import vueLogo from '/vue.svg'
+
+import { setupCounter } from './counter.ts'
+
+const btnCounter = useTemplateRef<HTMLButtonElement | null>('btn-counter')
+
+onMounted(() => {
+    setupCounter(btnCounter.value!)
+})
+</script>
+
+<template>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img :src="viteLogo" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://www.typescriptlang.org/" target="_blank">
+            <img :src="typescriptLogo" class="logo vanilla" alt="TypeScript logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img :src="vueLogo" class="logo vue" alt="Vue logo" />
+        </a>
+        <h1>Vite + TypeScript + Vue</h1>
+        <div class="card">
+            <button id="counter" type="button" ref="btn-counter"></button>
+        </div>
+        <p class="read-the-docs">
+            Click on the Vite and TypeScript logos to learn more
+        </p>
+    </div>
+</template>
