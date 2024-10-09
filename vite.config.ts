4584633fc7e599ec3fc7398dcbcb3a21b26be4a8
Noe Cortez <corteznoe3@gmail.com>
2024-10-09 14:16:39 -0600

    refactor: :zap: Agrega herramienta de desarrollo, pruebas y mejora la estructura del proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'pokemon-app',
-  plugins: [vue()],
+  base: '/pokemon-app/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
